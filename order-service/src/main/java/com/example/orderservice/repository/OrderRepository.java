b6d65551d597f41c533bebc197a40fdd93dcc7c3
Jomi Jacob <jomi.jacob.001@gmail.com>
2023-11-30 21:34:04 -0500

    feat: added tracing observability - Grafana

--- order-service/src/main/java/com/example/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/example/orderservice/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.example.orderservice.repository;
 
 import com.example.orderservice.model.Order;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Observed
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.productservice.repository;
 
 import com.example.productservice.model.Product;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Observed
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
